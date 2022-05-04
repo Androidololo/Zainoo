5885ac26d1b6949bfc4e7fd2eed9aa57f642c442
Androidololo <104149353+Androidololo@users.noreply.github.com>
2022-05-04 03:17:36 +0300

    js catalog

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
 import "../assets/styles.scss";
+let head = document.querySelector('header')
+head.innerHTML += `
+<a href="./index.html" class="header__title">Zainoo&trade;</a>
+<nav class="header__icons">
+    <img class="header__icon" src="./assets/img/search.svg">
+    <img class="header__icon" src="./assets/img/basket.svg">
+    <img class="header__icon" src="./assets/img/person.svg">
+</nav>
+`;
